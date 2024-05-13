64cdbe97cedd2a821f41ea1dd2b52c7ac8e4e2cf
harshini1708 <harshinipr17@gmail.com>
2024-05-13 14:00:00 -0700

    Bootstrap app and logic via hooks

---
+++ client/src/App.tsx
@@ -0,0 +1,49 @@
+import Landing from "./pages/Landing"
+import Signup from './pages/Signup';
+import { ThemeProvider } from "@/components/Theme/theme-provider"
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from "@/components/ui/sonner"
+import Signin from "./pages/Signin";
+import Home from "./pages/Home"
+import PrivacyPolicy from "./pages/Privacypolicy";
+import { BiLSTM } from "./pages/BiLstm";
+import { LSTM } from "./pages/Lstm";
+import Support from "./pages/support";
+import Learning from "./pages/Learning";
+import Watchlist from "./pages/Watchlist";
+import Profile from "./pages/Profile";
+import Feedback from "./pages/Feedback";
+import Prediction from "./pages/Prediction";
+import  { RandomForest } from "./pages/RandomForest";
+import { ARIMA } from "./pages/ARIMA";
+import ErrorPage from "./pages/ErrorPage";
+import { Analytics } from "@vercel/analytics/react"
+
+const App = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/profile/:username" element={<Profile />} /> 
+        <Route path="/signin" element={<Signin/>} /> 
+        <Route path="/home" element={<Home />} />
+        <Route path="/prediction-techniques" element={<Prediction />} />
+        <Route path="/prediction/bilstm" element={<BiLSTM />} /> 
+        <Route path="/prediction/lstm" element={<LSTM />} /> 
+        <Route path="/prediction/ARIMA" element={<ARIMA />} /> 
+        <Route path="/prediction/randomforest" element={<RandomForest />} /> 
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} /> 
+        <Route path="/learning" element={<Learning />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/watchlist/:username" element={<Watchlist />} />
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
+      <Toaster />
+      <Analytics />
+    </ThemeProvider>
+  )
+}
+
+export default App
